b9c4dcd2c45ec7c71de4264622cd859d01b79783
root <lepronet85@gmail.com>
2024-05-07 16:39:06 -0700

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of
-subscribers for a given subreddit"""
+"""
+0-main
+"""
+import sys
 
-import requests
-
-
-def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        return 0
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
